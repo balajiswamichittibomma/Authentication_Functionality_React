28450b6d4db8e9ec8f4ce26e6d27de063e1dd3d3
balaji <balajiswamichittibomma@gmail.com>
2023-05-01 08:20:07 +0000

    solution

---
+++ src/components/Header/index.js
@@ -0,0 +1,21 @@
+// Write your JS code here
+// Write your JS code here
+import {Link, withRouter} from 'react-router-dom'
+import './index.css'
+
+const Header = () => (
+  <nav>
+    <div>
+      <ul className="nav-menu">
+        <Link to="/" className="nav-link">
+          <li>Home</li>
+        </Link>
+        <Link to="/about" className="nav-link">
+          <li>About</li>
+        </Link>
+      </ul>
+    </div>
+  </nav>
+)
+
+export default withRouter(Header)
